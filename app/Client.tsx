c04cd71be6ab77fc7498331387f6abf6896c81c5
kratos-respawned <sbhandari1907@protonmail.com>
2024-02-17 12:40:59 +0530

    client server heading

--- app/Client.tsx
+++ app/Client.tsx
@@ -3,8 +3,8 @@
 export default function Client() {
   return (
     <div>
-      <h1>{process.env.SECRET}</h1>
-      <h1>{process.env.NEXT_PUBLIC_DATA}</h1>
+      <h1>Server Var :: {process.env.SECRET}</h1>
+      <h1>Client Var :: {process.env.NEXT_PUBLIC_DATA}</h1>
     </div>
   );
 }
